d1d8e5606cc4fbca309b41ce33d0f8733c71f82a
Rex  Sheikh <rexsheikh@rex-mac.local>
2025-06-07 22:50:49 -0400

    created styling for the navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 function Navbar() {
   return (
-    <nav style={{ padding: '1rem', background: '#ddd' }}>
+    <nav className="navbar">
       <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
       <Link to="/demos">Demos</Link>
       <Link to="/games">Games</Link>
