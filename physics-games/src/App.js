705ed1e4c9ae562930c00b71f648c30605d6d1be
Rex  Sheikh <rex.sheikh@gmail.com>
2025-06-22 00:51:56 -0400

    tested basic hello world div for arcade and demo pages.

--- physics-games/src/App.js
+++ physics-games/src/App.js
@@ -1,5 +1,27 @@
+// src/App.js
+
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Demo from './pages/Demo';
+import Arcade from './pages/Arcade';
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/demo">Demo</Link></li>
+            <li><Link to="/arcade">Arcade</Link></li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/demo" element={<Demo />} />
+          <Route path="/arcade" element={<Arcade />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
